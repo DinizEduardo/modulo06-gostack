732563724957268a5375b4134da8240b4ec6caa1
Eduardo Diniz <eduardo.diniz987@gmail.com>
2019-07-13 17:37:22 -0300

    Design da pagina main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
-
-// import { Container } from './styles';
+// import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
-  return <View />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuario"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#fff" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 Main.navigationOptions = {
