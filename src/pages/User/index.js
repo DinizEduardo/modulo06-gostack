c79a6304e4327d438b9b6d0cc6a4ab7c3eac8295
Eduardo Diniz <eduardo.diniz987@gmail.com>
2019-07-14 15:07:43 -0300

    Desafio do modulo 06

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { ActivityIndicator } from 'react-native';
 import api from '../../services/api';
 import {
   Container,
@@ -23,11 +24,15 @@ export default class User extends Component {
   static propTypes = {
     navigation: PropTypes.shape({
       getParam: PropTypes.func,
+      navigate: PropTypes.func,
     }).isRequired,
   };
 
   state = {
     stars: [],
+    loading: true,
+    page: 1,
+    refreshing: false,
   };
 
   async componentDidMount() {
@@ -36,12 +41,49 @@ export default class User extends Component {
 
     const response = await api.get(`/users/${user.login}/starred`);
 
-    this.setState({ stars: response.data });
+    this.setState({ stars: response.data, loading: false });
   }
+
+  loadMore = async () => {
+    const { stars, page } = this.state;
+    const { navigation } = this.props;
+    const nextPage = page + 1;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(
+      `/users/${user.login}/starred?page=${nextPage}`
+    );
+
+    this.setState({
+      stars: [...stars, ...response.data],
+      page: nextPage,
+    });
+  };
+
+  refreshList = async () => {
+    const { navigation } = this.props;
+    const nextPage = 1;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(
+      `/users/${user.login}/starred?page=${nextPage}`
+    );
+
+    this.setState({
+      stars: response.data,
+      page: nextPage,
+    });
+  };
+
+  handleNavigate = repository => {
+    const { navigation } = this.props;
+
+    navigation.navigate('Repository', { repository });
+  };
 
   render() {
     const { navigation } = this.props;
-    const { stars } = this.state;
+    const { stars, loading, refreshing } = this.state;
     const user = navigation.getParam('user');
     return (
       <Container>
@@ -50,20 +92,27 @@ export default class User extends Component {
           <Name>{user.name}</Name>
           <Bio>{user.bio}</Bio>
         </Header>
-
-        <Stars
-          data={stars}
-          keyExtractor={star => String(star.id)}
-          renderItem={({ item }) => (
-            <Starred>
-              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
-              <Info>
-                <Title>{item.name}</Title>
-                <Author>{item.owner.login}</Author>
-              </Info>
-            </Starred>
-          )}
-        />
+        {loading ? (
+          <ActivityIndicator color="#7159c1" size="large" />
+        ) : (
+          <Stars
+            onEndReachedThreshold={0.2} // Carrega mais itens quando chegar em 20% do fim
+            onEndReached={this.loadMore} // Função que carrega mais itens
+            onRefresh={this.refreshList} // Função dispara quando o usuário arrasta a lista pra baixo
+            refreshing={refreshing} // Variável que armazena um estado true/false que representa se a lista está atualizando
+            data={stars}
+            keyExtractor={star => String(star.id)}
+            renderItem={({ item }) => (
+              <Starred onPress={() => this.handleNavigate(item)}>
+                <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        )}
       </Container>
     );
   }
